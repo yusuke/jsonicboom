e22cc0f2c38ef174441ddff06473775c9694f1b4
Yusuke Yamamoto <yusuke@mac.com>
2023-03-29 10:24:39 +0900

    IDEA files

---
+++ module-info/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * <a href="https://github.com/yusuke/jsonicboom">JSON Library
+ * </a>
+ */
+module one.cafebabe.jsonicboom{
+    exports one.cafebabe.jsonicboom;
+    requires java.logging;
+    requires static org.slf4j;
+    requires static org.jetbrains.annotations;
+}
